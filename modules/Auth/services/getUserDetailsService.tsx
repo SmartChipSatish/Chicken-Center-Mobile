98d0575e7ebd6a72588cfc85daceaa0d1217febe
Saiprakash <talakantisaiprakash@gmail.com>
2024-05-28 19:46:29 +0530

    added update profile

--- modules/Auth/services/getUserDetailsService.tsx
+++ modules/Auth/services/getUserDetailsService.tsx
@@ -9,9 +9,26 @@ export const getUserData = userApi.injectEndpoints({
                     method: 'GET'
                 }),
             invalidatesTags: ['userList']
+        }),
+        getUserByUserId: builder.mutation({
+            query: (userId) => (
+                {
+                    url: `/getUserById?userId=${userId}`,
+                    method: 'GET'
+                }),
+            invalidatesTags: ['userList']
+        }),
+        updateUser: builder.mutation({
+            query: ({ userId, ...rest }) => (
+                {
+                    url: `/updateUser?userId=${userId}`,
+                    method: 'POST',
+                    body: rest
+                }),
+            invalidatesTags: ['userList']
         })
     }),
     overrideExisting: true,
 })
 
-export const { useGetUserDetailsMutation } = getUserData
+export const { useGetUserDetailsMutation, useGetUserByUserIdMutation, useUpdateUserMutation } = getUserData
